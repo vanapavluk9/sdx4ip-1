b00f9ea882974e1ece77fc73194e332d6c9fcfa2
VANAPAVLUK9😎😉 <159788937+vanapavluk9@users.noreply.github.com>
2026-04-26 12:10:07 +0300

    Update installingdebianinphonexfce4.sh

--- installingdebianinphonexfce4.sh
+++ installingdebianinphonexfce4.sh
@@ -8,7 +8,7 @@ apt install x11-repo tur-repo -y && apt install termux-x11-nightly pulseaudio pr
 
 pd install debian
 
-proot-distro login debian --shared-tmp -- /bin/bash -c 'apt update -y && apt upgrade -y && apt install xfce4 xfce4-goodies sudo nano chromium git -y && echo "You need to setup password, but remember your password (if you dont enter password you cant install apps)" && sleep 3 && adduser debianuser && mv "/usr/bin/chromium" "/usr/bin/chrim" && echo "chrim --no-sandbox" > /usr/bin/chromium && chmod +x /usr/bin/chromium && rm /etc/sudoers && git clone https://github.com/vanapavluk9/sdx4ip-1.git && mv sdx4ip-1/sudoers /etc && chmod +x /etc/sudoers && exit'
+proot-distro login debian --shared-tmp -- /bin/bash -c 'apt update -y && apt upgrade -y && apt install xfce4 xfce4-goodies sudo nano chromium git -y && echo "You need to setup password, but remember your password (if you dont enter password you cant install apps)" && sleep 3 && adduser debianuser && mv "/usr/bin/chromium" "/usr/bin/chrim" && echo "chrim --no-sandbox" > /usr/bin/chromium && chmod +x /usr/bin/chromium && rm /etc/sudoers && git clone https://github.com/vanapavluk9/sdx4ip-1.git && mv sdx4ip-1/sudoers /etc && chmod +x /etc/sudoers && rm -r sdx4ip-1 && exit'
 
 chmod +x sdx4ip-1/startxfce4
 
